add90cddaf4bc5a74f0731bbbdaef34501e47b2e
sono8stream <sono.stream8580@gmail.com>
2020-01-18 16:34:49 +0900

    Add semantic ui style by default

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
+import 'semantic-ui-css/semantic.min.css';
 import './App.css';
+import logo from './logo.svg';
 
 const App: React.FC = () => {
   return (
